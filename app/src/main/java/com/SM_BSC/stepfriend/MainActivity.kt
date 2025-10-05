1bf007b7c329a90982f0e7a60832214b588b8d31
Saul Maylin <96966989+MrJesterBear@users.noreply.github.com>
2025-10-05 02:29:43 +0100

    Added Dependency (viewmodel)
    - Added  implementation(libs.androidx.lifecycle.viewmodel.compose)
    
    - note for future self though, i had to do a bunch of quick fixes to get there by starting with implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.0")

--- app/src/main/java/com/SM_BSC/stepfriend/MainActivity.kt
+++ app/src/main/java/com/SM_BSC/stepfriend/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.SM_BSC.stepfriend.ui.models.MainViewModel
 import com.SM_BSC.stepfriend.ui.theme.StepFriendTheme
 
